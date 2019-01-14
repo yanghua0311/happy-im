268945c0a88cecfd3e5229f626d068a91fe4fa9c
youngwa <87965110@qq.com>
2019-01-14 21:07:28 +0800

    init happy im

---
+++ src/main/java/com/happy/im/happyim/HappyImApplication.java
@@ -0,0 +1,14 @@
+package com.happy.im.happyim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyImApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyImApplication.class, args);
+	}
+
+}
+
